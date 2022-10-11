9c2885c758ae59f743ef8c938a5b4e0248191790
Carlos Arturo Diaz <70271822+ArturoDiaz02@users.noreply.github.com>
2022-10-11 08:30:03 -0500

    Main to Login and Perfil

--- app/src/main/java/com/example/smart_insurance/LoginActivity.kt
+++ app/src/main/java/com/example/smart_insurance/LoginActivity.kt
@@ -1,11 +1,30 @@
 package com.example.smart_insurance
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.smart_insurance.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener{
+            goMain()
+        }
+
+        binding.button3.setOnClickListener{
+            goMain()
+        }
+    }
+
+    private fun goMain(){
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/smart_insurance/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/smart_insurance/fragments/HomeFragment.kt
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.smart_insurance.adapter.CardAdapter
 import com.example.smart_insurance.databinding.FragmentHomeBinding
@@ -38,11 +37,12 @@ class HomeFragment : Fragment(), CardAdapter.OnItemClickListener {
 
     companion object{
         @JvmStatic
-        fun newInstance() = HomeFragment()
+       fun newInstance() = HomeFragment()
+
     }
 
     override fun onItemClick(position: Int) {
-        Toast.makeText(activity, "Item $position clicked", Toast.LENGTH_SHORT).show()
+
     }
 
 }
