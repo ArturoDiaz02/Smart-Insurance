1aa9660055fca1b9ff7c8fd00f9ddf8144c33c73
Cristian Sanchez <crissanchezpelaez@gmail.com>
2022-10-16 17:19:57 -0500

    Added configure group

---
+++ app/src/main/java/com/example/smart_insurance/ConfigureGroup.kt
@@ -0,0 +1,26 @@
+package com.example.smart_insurance
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.smart_insurance.databinding.ActivityConfigureGroupBinding
+
+class ConfigureGroup : AppCompatActivity() {
+
+    private lateinit var binding: ActivityConfigureGroupBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityConfigureGroupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        
+        binding.backButton.setOnClickListener{
+            goBack()
+        }
+    }
+
+    private fun goBack(){
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+    }
+}
